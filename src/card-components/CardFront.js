a948a75eafa5df4276e172a6cd6a7b188348f2ae
s-satsangi <s.satsangi@gmail.com>
2020-12-09 22:52:47 -0500

    Sheesh

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+// {console.log(this.props.title)}
+console.log("1")
 export default class CardFront extends Component {
-
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
     )
   }
